d8a01e27f5af0b3677c8c812fd720626d22d7580
iiiqwertyiii <nicpabe@gmail.com>
2020-12-09 19:11:10 -0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#homepage'
+  scope 'artists' do
+    get '/', to:'artists#index', as: :artists
+    get '/new', to:'artists#new', as: :new_artist
+    post '/new', to:'artists#create'
+  end
 end
